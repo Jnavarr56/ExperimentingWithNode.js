fdde2a0627707ec880f7b2262fa6383eab435a50
Jorge Navarro <jnavarr56@gmail.com>
2019-02-26 01:25:29 -0500

    first

---
+++ myFirst.js
@@ -0,0 +1,25 @@
+//Experimenting with Node.js
+/*
+    So Far: 
+     - You used NPM to install axios and are doing a simple get request to get the reddit homepage data as JSON.
+*/
+
+const axios = require('axios');
+
+axios.get('https://www.reddit.com/.json').then(response => {
+
+    response.data.data.children.forEach(l => {
+
+        console.log(
+            {
+                title: l.data.title, 
+                subreddit: `r/${l.data.subreddit}`, 
+                link: `reddit.com${l.data.permalink}`
+            }
+        );
+
+    });
+
+});
+    
+
