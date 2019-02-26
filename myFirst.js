57134ad424fa87e32274c770bf2434d212a17fcf
Jorge Navarro <jnavarr56@gmail.com>
2019-02-26 01:28:13 -0500

    added w3 schools tutorial link.

--- myFirst.js
+++ myFirst.js
@@ -1,4 +1,6 @@
 //Experimenting with Node.js
+//==========================
+
 /*
     So Far: 
      - You used NPM to install axios and are doing a simple get request to get the reddit homepage data as JSON.
@@ -11,15 +13,22 @@ axios.get('https://www.reddit.com/.json').then(response => {
     response.data.data.children.forEach(l => {
 
         console.log(
+
             {
                 title: l.data.title, 
                 subreddit: `r/${l.data.subreddit}`, 
                 link: `reddit.com${l.data.permalink}`
             }
+
         );
 
     });
 
 });
+
+//Continue with the W3Schools Node.js Tutorial https://www.w3schools.com/nodejs/nodejs_modules.asp
+
+
+
     
 
