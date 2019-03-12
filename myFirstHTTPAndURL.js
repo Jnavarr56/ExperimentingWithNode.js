73e5b64aa96775cefc74fbaaea41ca9cffba5932
Jorge Navarro <jnavarr56@gmail.com>
2019-03-11 23:29:40 -0400

    Added URL module experimentation.

--- myFirstHTTPAndURL.js
+++ myFirstHTTPAndURL.js
@@ -13,34 +13,17 @@ const http = require('http');   //HTTP Module
 //Created my own module.
 const myCustomModule = require('./myTestModule'); 
 
+console.log('Running on port 8080...');
 
 //Experimenting with HTTP Module--------------
 http.createServer(function (req, res) {
 
     const urlObj = url.parse(req.url, true); //<-- Experimenting with URL Module FOR MORE ON THIS SEE BOTTOM
 
-    if (urlObj.path === '/') {
+    res.writeHead(404, {'Content-Type': 'text/html'});
 
-        fs.readFile('index.html', function(err, data) {
+    if (urlObj.path === '/get-current-time') {
 
-            if (err) {
-
-                res.writeHead(404, {'Content-Type': 'text/html'});
-                return res.end('404 Not Found');
-
-            }
-
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(data);
-            return res.end();
-
-        }); 
-
-    }
-
-    else if (urlObj.path === '/get-current-time') {
-
-        res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(`<p style="font-family: Helvetica;">This is the current date and time: ${myCustomModule.getCurrentTime()}</p>`);
         return res.end();
 
@@ -48,29 +31,37 @@ http.createServer(function (req, res) {
 
     else {
 
+        const fullUrl = req.headers.host + req.url;
 
-        res.writeHead(301,
-            { Location: '/' }
-        );
-        return res.end();
+        fs.readFile('index.html', function(err, data) {
+
+            if (err) {
+ 
+                return res.end('404 Not Found');
+
+            }
+
+            res.write(data);
+
+            if (fullUrl.split('/')[1].length !== '') {
+
+                const urlParseObj = url.parse(fullUrl, true);
+
+                if (urlParseObj.search) {
+
+                    res.write(`<p style="font-family: Helvetica;">url.parse(${fullUrl}, true).search returns ${urlParseObj.search}</p>`);
+                    res.write(`<p style="font-family: Helvetica;">url.parse(${fullUrl}, true).query returrns ${JSON.stringify(urlParseObj.query)}</p>`);
+
+                }
+                
+            }
+
+            return res.end();
+            
+        }); 
 
     }
 
 
 }).listen(8080);
 
-//Continue with the W3Schools Node.js Tutorial https://www.w3schools.com/nodejs/nodejs_filesystem.asp
-
-
-/*
-var url = require('url');
-var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-var q = url.parse(adr, true);
-
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
-
-var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata.month); //returns 'february'
-*/
